a6d50aa19ec61e16a75bc185b032c03ec2ca59ef
ChamRong <Suonsokchamrong041104@gmail.com>
2024-12-24 10:20:35 +0700

    update

--- js/script.js
+++ js/script.js
@@ -61,3 +61,22 @@ document.addEventListener("click", e => {
     normalHandStyle();
   }
 });
+
+document.onkeydown = function(e) {
+  if(e.keyCode == 123) {
+   return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+   return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+   return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+   return false;
+  }
+
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){
+   return false;
+  }      
+}
